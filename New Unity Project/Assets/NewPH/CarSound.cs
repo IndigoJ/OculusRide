6c4115223f37f6ae8bf6e2514f47be6cfda8eefb
Altaro <altarox@gmail.com>
2014-12-14 21:59:55 +0200

    Trash removed

--- New Unity Project/Assets/NewPH/CarSound.cs
+++ New Unity Project/Assets/NewPH/CarSound.cs
@@ -17,7 +17,7 @@ public class CarSound : MonoBehaviour {
 
 	private void EngineSound(){
 		if (NC.currentGear > 0) {
-			audio.pitch = ((NC.rpm - 1500) / (NC.changGears [NC.currentGear - 1] - 1500))+1;
+			audio.pitch = ((NC.rpm - 1500) / (NC.changGears [NC.currentGear - 1] - 1500))+0.3f;
 		}
 		else {
 			audio.pitch = 0.1f;
