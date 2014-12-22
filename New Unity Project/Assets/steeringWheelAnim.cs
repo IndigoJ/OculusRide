59f84404bd4a13511306d6760076207479e8bfe5
Altaro <altarox@gmail.com>
2014-12-23 01:40:12 +0200

    Commit message

---
+++ New Unity Project/Assets/steeringWheelAnim.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class steeringWheelAnim : MonoBehaviour {
+
+	public float NeedAngle;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localEulerAngles = Vector3.Lerp (transform.localEulerAngles, new Vector3 (19.52147f, 0, NeedAngle), 0.5f);
+	}
+}
